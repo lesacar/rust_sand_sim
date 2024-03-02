d27923d450b846c37ceaf3596a7fef309b179769
lesacar <balkangamescroatia@gmail.com>
2024-03-02 02:12:30 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ use raylib_ffi::*;
 
 mod setup;
 fn main() {
-    setup::setup_stuff(1440, 720, "Rust Raylib window".to_string(), 3, false);
+    setup::setup_stuff(1440, 720, "Rust Raylib window".to_string(), 4, false);
     setup::set_monitor_and_fps(1);
     unsafe {
         while !WindowShouldClose() {
             BeginDrawing();
             ClearBackground(raylib_ffi::colors::BLACK);
+            DrawText(
+                TextFormat(rl_str!(GetFPS())),
+                20,
+                20,
+                20,
+                raylib_ffi::colors::WHITE,
+            );
             EndDrawing();
         }
     }
