670c5ed0e9d006278ac976bf18ac47ace19d6879
lesacar <balkangamescroatia@gmail.com>
2024-03-02 22:35:27 +0100

    able to draw

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use raylib_ffi::*;
+use raylib_ffi::{
+    enums::{KeyboardKey, MouseButton},
+    *,
+};
 use setup::BLOCK_SIZE;
 
 mod setup;
 
 fn main() {
+    let mut mscroll_brush_size: i32 = 10;
     const SCREEN_WIDTH: i32 = 1440;
     const SCREEN_HEIGHT: i32 = 720;
     setup::setup_stuff(
@@ -13,44 +17,79 @@ fn main() {
         4,
         false,
     );
-    const ROWS: usize = (SCREEN_HEIGHT / BLOCK_SIZE) as usize;
-    const COLS: usize = (SCREEN_WIDTH / BLOCK_SIZE) as usize;
+    const ROWS: u32 = (SCREEN_HEIGHT / BLOCK_SIZE) as u32;
+    const COLS: u32 = (SCREEN_WIDTH / BLOCK_SIZE) as u32;
 
     setup::set_monitor_and_fps(1);
-    let brush_mode: bool = true;
-    let mut grid: Vec<Vec<setup::Cell>> = vec![vec![setup::Cell::default(); ROWS]; COLS];
+    let mut brush_mode: bool = true;
+    let mut grid: Vec<Vec<setup::Cell>> = Vec::with_capacity(ROWS as usize);
+    for _ in 0..ROWS {
+        let row: Vec<setup::Cell> = vec![setup::Cell::default(); COLS as usize];
+        grid.push(row);
+    }
+
+    let mut draw_mat = setup::Material::Empty;
     unsafe {
         while !WindowShouldClose() {
-            if IsMouseButtonDown(0) {
+            if IsMouseButtonDown(MouseButton::Left as i32) {
                 // Left Mouse Button
                 let mx = GetMouseX();
                 let my = GetMouseY();
-                let grid_x = (mx / BLOCK_SIZE) as usize;
-                let grid_y = (my / BLOCK_SIZE) as usize;
+                let grid_x = (mx / BLOCK_SIZE) as u32;
+                let grid_y = (my / BLOCK_SIZE) as u32;
+                if grid_x < COLS && grid_y < ROWS {
+                    setup::spawn_sand_brush(
+                        &mut grid, mx, my, 50,
+                        draw_mat, // Set material to Sand (assuming Sand corresponds to material 1)
+                        brush_mode,
+                    );
+                }
+            } else if IsMouseButtonDown(MouseButton::Right as i32) {
+                // Left Mouse Button
+                let mx = GetMouseX();
+                let my = GetMouseY();
+                let grid_x = (mx / BLOCK_SIZE) as u32;
+                let grid_y = (my / BLOCK_SIZE) as u32;
                 if grid_x < COLS && grid_y < ROWS {
                     setup::spawn_sand_brush(
                         &mut grid,
                         mx,
                         my,
-                        10,
-                        setup::Material::Sand, // Set material to Sand (assuming Sand corresponds to material 1)
-                        brush_mode,
+                        mscroll_brush_size,
+                        setup::Material::Empty, // Set material to Sand (assuming Sand corresponds to material 1)
+                        false,
                     );
                 }
             }
+            if IsKeyPressed(KeyboardKey::One as i32) {
+                draw_mat = setup::Material::Sand;
+            }
+            if IsKeyPressed(KeyboardKey::Two as i32) {
+                draw_mat = setup::Material::Water;
+            }
+            if IsKeyPressed(KeyboardKey::Three as i32) {
+                draw_mat = setup::Material::Stone;
+            }
+            if IsKeyPressed(KeyboardKey::B as i32) {
+                brush_mode = !brush_mode;
+            }
+            mscroll_brush_size += GetMouseWheelMove() as i32 * 3;
+            if mscroll_brush_size < 1 {
+                mscroll_brush_size = 1;
+            }
+            println!("{}", mscroll_brush_size);
 
             BeginDrawing();
             ClearBackground(raylib_ffi::colors::BLACK);
-            for i in 0..COLS {
-                for j in 0..ROWS {
-                    if grid[i][j].material != setup::Material::Empty {
-                        let color = grid[i][j].color;
+            for i in 0..ROWS {
+                for j in 0..COLS {
+                    if grid[i as usize][j as usize].material != setup::Material::Empty {
                         DrawRectangle(
-                            (i as i32 * BLOCK_SIZE) as i32,
-                            (j as i32 * BLOCK_SIZE) as i32,
+                            (j as f32 * BLOCK_SIZE as f32) as i32,
+                            (i as f32 * BLOCK_SIZE as f32) as i32,
                             BLOCK_SIZE as i32,
                             BLOCK_SIZE as i32,
-                            color.into(),
+                            grid[i as usize][j as usize].color,
                         );
                     }
                 }
--- src/setup.rs
+++ src/setup.rs
@@ -1,5 +1,5 @@
 // setup.rs
-use rand::Rng;
+// use rand::Rng;
 use raylib_ffi::*;
 
 pub const BLOCK_SIZE: i32 = 4; // Example block size
@@ -57,7 +57,7 @@ pub fn spawn_sand_brush(
     let rows = grid.len() as i32;
     let cols = grid[0].len() as i32;
 
-    let brush_radius = brush_size / 2;
+    let brush_radius: i32 = brush_size as i32 / 2;
 
     let start_x = (mouse_x / BLOCK_SIZE) - brush_radius;
     let start_y = (mouse_y / BLOCK_SIZE) - brush_radius;
@@ -65,48 +65,96 @@ pub fn spawn_sand_brush(
     let start_x = start_x.max(0);
     let start_y = start_y.max(0);
 
-    let end_x = (start_x + brush_size).min(cols);
-    let end_y = (start_y + brush_size).min(rows);
+    let center_x = (start_x + brush_radius) as f32;
+    let center_y = (start_y + brush_radius) as f32;
 
-    for i in start_x..end_x {
-        for j in start_y..end_y {
-            let distance = (((i - start_x).pow(2) + (j - start_y).pow(2)) as f32).sqrt();
-
-            if distance <= brush_radius as f32 {
-                if let Some(cell) = grid.get_mut(i as usize) {
-                    if let Some(cell) = cell.get_mut(j as usize) {
-                        cell.material = material;
-                        match material {
-                            Material::Sand => {
-                                cell.friction = 0.95;
-                                cell.color = Color {
-                                    r: 201,
-                                    g: 170,
-                                    b: 127,
-                                    a: 255,
-                                };
+    for i in start_y..start_y + brush_size {
+        for j in start_x..start_x + brush_size {
+            let distance = (((j as f32 - center_x as f32 + 0.5).powf(2.0f32)
+                + (i as f32 - center_y as f32 + 0.5).powf(2.0f32))
+                as f32)
+                .sqrt();
+            if i >= 0 && i < rows && j >= 0 && j < cols {
+                if material == Material::Empty {
+                    grid[i as usize][j as usize] = Cell::default(); // line 83
+                } else if let Some(cell_row) = grid.get_mut(i as usize) {
+                    if let Some(cell) = cell_row.get_mut(j as usize) {
+                        if brush_mode {
+                            if brush_size == 1
+                                || (rand::random::<f32>() > 0.8 && distance <= brush_radius as f32)
+                            {
+                                // Check if the current cell is empty before setting its material
+                                if cell.material == Material::Empty {
+                                    cell.material = material;
+                                    cell.material = material;
+                                    match material {
+                                        Material::Sand => {
+                                            cell.friction = 0.95;
+                                            cell.color = Color {
+                                                r: 201,
+                                                g: 170,
+                                                b: 127,
+                                                a: 255,
+                                            };
+                                        }
+                                        Material::Water => {
+                                            cell.friction = 0.0;
+                                            cell.spread_factor = 5;
+                                            cell.color = Color {
+                                                r: 0,
+                                                g: 0,
+                                                b: 255,
+                                                a: 255,
+                                            };
+                                        }
+                                        Material::Stone => {
+                                            cell.friction = 0.0;
+                                            cell.spread_factor = 0;
+                                            cell.color = Color {
+                                                r: 51,
+                                                g: 83,
+                                                b: 69,
+                                                a: 255,
+                                            };
+                                        }
+                                        _ => {}
+                                    }
+                                }
                             }
-                            Material::Water => {
-                                cell.friction = 0.0;
-                                cell.spread_factor = 5;
-                                cell.color = Color {
-                                    r: 0,
-                                    g: 0,
-                                    b: 255,
-                                    a: 255,
-                                };
+                        } else if distance <= brush_radius as f32 {
+                            cell.material = material;
+                            match material {
+                                Material::Sand => {
+                                    cell.friction = 0.95;
+                                    cell.color = Color {
+                                        r: 201,
+                                        g: 170,
+                                        b: 127,
+                                        a: 255,
+                                    };
+                                }
+                                Material::Water => {
+                                    cell.friction = 0.0;
+                                    cell.spread_factor = 5;
+                                    cell.color = Color {
+                                        r: 0,
+                                        g: 0,
+                                        b: 255,
+                                        a: 255,
+                                    };
+                                }
+                                Material::Stone => {
+                                    cell.friction = 0.0;
+                                    cell.spread_factor = 0;
+                                    cell.color = Color {
+                                        r: 51,
+                                        g: 83,
+                                        b: 69,
+                                        a: 255,
+                                    };
+                                }
+                                _ => {}
                             }
-                            Material::Stone => {
-                                cell.friction = 0.0;
-                                cell.spread_factor = 0;
-                                cell.color = Color {
-                                    r: 51,
-                                    g: 83,
-                                    b: 69,
-                                    a: 255,
-                                };
-                            }
-                            _ => {}
                         }
                     }
                 }
